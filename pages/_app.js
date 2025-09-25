b4ab742ef16a7b1bd2235126e04e33c58b5af2b2
최진섭 <choijinseob@choejinseob-ui-MacBookPro.local>
2025-09-25 16:38:02 +0900

    feat: Integrate Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      // Track page views for Google Analytics
+      if (window.gtag) {
+        window.gtag('config', 'G-VFQQFB520E', {
+          page_path: url,
+        });
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko" data-scroll-behavior="smooth">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VFQQFB520E"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VFQQFB520E');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
